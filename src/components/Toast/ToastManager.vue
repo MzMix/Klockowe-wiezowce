d53874dfd3b65653c5d4d5f38fd87fd92dbba876
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-09-19 19:10:54 +0200

    feat(UI): :sparkles: add toast component with trigger function
    add toast component with trigger function

---
+++ src/components/Toast/ToastManager.vue
@@ -0,0 +1,52 @@
+<script setup>
+import bsToast from './bsToast.vue';
+import { inject, onMounted } from 'vue';
+const ShowToast = inject('ToastTrigger');
+onMounted(() => {
+    for (let i = 1; i <= 8; i++) {
+        ShowToast(`#t${i}`)
+    }
+})
+</script>
+    
+<template>
+
+    <div aria-live="polite" aria-atomic="true" class="" id="ToastGroup">
+        <div class="toast-container bottom-0 end-0 p-3">
+
+            <bsToast Theme="white" Name="t1">
+                <template #header>Toast #1</template>
+            </bsToast>
+            <bsToast Theme="primary" Name="t2">
+                <template #header>Toast #2</template>
+            </bsToast>
+            <bsToast Theme="secondary" Name="t3">
+                <template #header>Toast #3</template>
+            </bsToast>
+            <bsToast Theme="purple" Name="t4">
+                <template #header>Toast #4</template>
+            </bsToast>
+            <bsToast Theme="success" Name="t5">
+                <template #header>Toast #5</template>
+            </bsToast>
+            <bsToast Theme="danger" Name="t6">
+                <template #header>Toast #6</template>
+            </bsToast>
+            <bsToast Theme="warning" Name="t7">
+                <template #header>Toast #7</template>
+            </bsToast>
+            <bsToast Theme="info" Name="t8">
+                <template #header>Toast #8</template>
+            </bsToast>
+
+        </div>
+    </div>
+
+</template>
+    
+<style scoped>
+#ToastGroup {
+    top: 0;
+    right: 0;
+}
+</style>
---
+++ src/components/Toast/bsToast.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    Theme: {
+        required: false,
+        type: String,
+        default: 'white',
+        validator(value) {
+            return ['white', 'purple', 'primary', 'secondary', 'success', 'danger', 'warning', 'info'].includes(value)
+        }
+    },
+    Name: {
+        required: true,
+        type: String,
+        default: `Toast-${Date.now()}`
+    }
+})
+const Theme = computed(() => {
+    let result = `bg-${props.Theme} `;
+    if (props.Theme === 'white') {
+        result += 'text-black';
+    } else {
+        result += 'text-white'
+    }
+    return result;
+})
+</script>
+    
+<template>
+
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" :id="props.Name">
+
+        <div class="toast-header" :class="Theme">
+            <div class="me-2">
+                <slot name="icon"><i class="bi bi-info-circle"></i></slot>
+            </div>
+            <strong class="me-auto">
+                <slot name="header">Bootstrap</slot>
+            </strong>
+            <small>
+                <slot name="time">11 mins ago</slot>
+            </small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+
+        <div class="toast-body bg-white">
+            <slot name="body">Hello, world! This is a toast message.</slot>
+        </div>
+    </div>
+
+</template>
+    
+<style scoped>
+
+</style>
